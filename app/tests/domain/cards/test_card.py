2f934de3d05e03ca76edbf1b7c78319b96b47a3a
Maycol Teles <maycolteles@hotmail.com>
2022-07-28 00:25:41 -0300

    test(Card representation): Fixing the expected Card string representation.

--- app/tests/domain/cards/test_card.py
+++ app/tests/domain/cards/test_card.py
@@ -4,9 +4,7 @@ Module containing the 'TestCard' Class.
 
 import unittest
 
-from src.domain.cards.card import Card
-from src.domain.cards.suits import Suits
-from src.domain.cards.values import Values
+from src.domain.cards import Card, Suits, Values
 
 
 class TestCard(unittest.TestCase):
@@ -14,9 +12,9 @@ class TestCard(unittest.TestCase):
     Class to test the 'Card' Class.
     """
 
-    def test_card_construct(self) -> None:
+    def test_card_constructor(self) -> None:
         """
-        Method to test the '__new__' (or constructor) method.
+        Method to test the '__init__' (or constructor) method.
         """
         test_card_value = Values.ACE
         test_card_suit = Suits.CLUB
@@ -38,6 +36,6 @@ class TestCard(unittest.TestCase):
 
         actual = str(Card(card_value=test_card_value, card_suit=test_card_suit))
 
-        expected = f"{test_card_value.value} of {test_card_suit.value}."
+        expected = f"{test_card_value.value} of {test_card_suit.value}"
 
         self.assertEqual(actual, expected)
