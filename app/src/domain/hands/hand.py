716f2bca4e8e670e55bad9510c72ed431a6861b1
Maycol Teles <maycolteles@hotmail.com>
2022-07-28 00:37:54 -0300

    feat(Hand): Implementing the Hand entity class.

---
+++ app/src/domain/hands/hand.py
@@ -0,0 +1,36 @@
+"""
+Module containing the 'Hand' Class.
+"""
+
+# TYPING IMPORTS
+from typing import List
+
+from ..cards import Card
+
+# MODULE IMPORTS
+from dataclasses import dataclass
+
+
+@dataclass
+class Hand:
+    """
+    Class to represent a Poker Hand.
+
+    Attributes
+    ----------
+    cards : List[Cards]
+    """
+
+    cards: List[Card]
+
+    def __str__(self) -> str:
+        """
+        Method to configure how the class will be printed.
+
+        Returns
+        --------
+        str
+            The representation of the class in str format.
+        """
+        # TODO: SUPPORT MORE CARDS IN HAND
+        return f"{self.cards[0]} and {self.cards[1]}"
